9ed2e7f0c26a0d62be442a4fe151213aad9ded41
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-07-11 16:23:08 +0900

    updated bottom navbar height

--- lib/components/bottom_navbar.dart
+++ lib/components/bottom_navbar.dart
@@ -11,52 +11,56 @@ class BottomNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTap,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: const Color.fromARGB(69, 158, 158, 158),
-      selectedItemColor: Colors.white,
-      selectedLabelStyle: TextStyle(color: Colors.white),
-      unselectedItemColor: const Color.fromARGB(255, 158, 158, 158),
-      items: [
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.home,
+    return SizedBox(
+      height: 120,
+      child: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onTap,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color.fromARGB(69, 158, 158, 158),
+        selectedItemColor: Colors.white,
+        selectedLabelStyle: TextStyle(color: Colors.white),
+        unselectedItemColor: const Color.fromARGB(255, 158, 158, 158),
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 32,
+            ),
+            label: "Home",
           ),
-          label: "Home",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.people,
-            color: Colors.grey.shade300,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.people,
+              size: 32,
+            ),
+            label: "Social",
           ),
-          label: "Social",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.search,
-            color: Colors.grey.shade300,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              size: 32,
+            ),
+            label: "Search",
           ),
-          label: "Search",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.library_books_outlined,
-            color: Colors.grey.shade300,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.library_books_outlined,
+              size: 32,
+            ),
+            label: "Library",
           ),
-          label: "Library",
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(
-            Icons.games_outlined,
-            color: Colors.grey.shade300,
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.games_outlined,
+              size: 32,
+            ),
+            label: "Games",
           ),
-          label: "Games",
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
