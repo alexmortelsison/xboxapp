11808ca9137f852612eccf7d48dceb2d5cceb86d
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-07-19 17:23:47 +0900

    added searchbar

--- lib/pages/search_screen.dart
+++ lib/pages/search_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:xboxapp/components/top_appbar.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Search"));
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: CustomScrollView(
+        slivers: [
+          TopAppbar(),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Search",
+                    style: TextStyle(color: Colors.white, fontSize: 28),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 16),
+                    child: SearchBar(
+                      hintText: "Search",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/social_screen.dart
+++ lib/pages/social_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:xboxapp/components/top_appbar.dart';
 
 class SocialScreen extends StatelessWidget {
   const SocialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Social"));
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      body: CustomScrollView(
+        slivers: [
+          TopAppbar(),
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Image.asset(
+                  "lib/assets/friends.png",
+                  height: 350,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
